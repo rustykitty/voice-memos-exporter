6c9fbfe5eb414aa861b40f0e65306cfa2ce81953
Rusty <rusty3925@gmail.com>
2026-05-13 20:12:07 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import sqlite3
+import sys
+import os
+import os.path
+
+from collections import namedtuple, Counter
+import datetime
+import time
+import shutil
+
+import util
+
+# macOS storage locations as of macOS 15 Sequoia
+DEFAULT_DB_FILENAME = os.path.expanduser("~/Library/Group Containers/group.com.apple.VoiceMemos.shared/Recordings/CloudRecordings.db")
+DEFAULT_RECORDINGS_FOLDER = os.path.expanduser("~/Library/Group Containers/group.com.apple.VoiceMemos.shared/Recordings/")
+OUTPUT_DIRECTORY = "out"
+
+def main():
+    # args = sys.argv
+    # if len(args) < 2:
+    #     print("You must pass the filename as an argument to the function.")
+    #     return 1
+
+    # filename = args[1]
+
+    db_filename = DEFAULT_DB_FILENAME
+
+    conn = sqlite3.connect(db_filename)
+
+    cursor = conn.cursor()
+
+    Row = namedtuple("Row", "name date filename folder")
+
+    cursor.execute("SELECT ZCLOUDRECORDING.ZENCRYPTEDTITLE, ZDATE, ZPATH, ZFOLDER.ZENCRYPTEDNAME " \
+                   "FROM ZCLOUDRECORDING LEFT JOIN ZFOLDER ON ZCLOUDRECORDING.ZFOLDER = ZFOLDER.Z_PK ORDER BY ZDATE ASC;")
+    result = cursor.fetchall()
+    
+    result = [
+        Row(*row) for row in result
+    ]
+
+    for row in result:
+        print(row)
+
+    util.mkdir_exist_ok(OUTPUT_DIRECTORY)
+
+    # for name collisions
+    name_counter = Counter()
+
+    table = []
+
+    for row in result:
+        name, date, filename, folder = row
+
+        if folder:
+            folder = util.escape(folder)
+            util.mkdir_exist_ok(os.path.join(OUTPUT_DIRECTORY, folder))
+            output_filename = os.path.join(folder, util.escape(name))
+        else:
+            output_filename = util.escape(name)
+
+        output_filename = os.path.join(OUTPUT_DIRECTORY, output_filename) + ".m4a"
+
+        timestamp = util.convert_timestamp(date)
+
+        # todo: split across multiple lines
+        table.append("\t".join(util.non_nullify((name, folder, util.iso8601(timestamp)))))
+
+        # actually write the file now
+        filename = os.path.join(DEFAULT_RECORDINGS_FOLDER, filename)
+
+        shutil.copy(filename, output_filename)
+        # (atime, mtime)
+        os.utime(output_filename, times = (time.time(), timestamp))
+
+    table_txt = "\n".join(table)
+    with open(os.path.join(OUTPUT_DIRECTORY, "data.tsv"), "w") as fp:
+        fp.write(table_txt)
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ util.py
@@ -0,0 +1,32 @@
+import datetime
+
+from os import mkdir
+from contextlib import suppress
+
+def convert_timestamp(timestamp):
+    return timestamp + 978307200
+
+def escape(s: str):
+    # I'm not gonna escape anything else because you shouldn't be running
+    # this program on anything other than macOS.
+    # You shouldn't be putting tabs in your Voice Memo names, but if you do, it
+    # messes with the TSV that this program outputs.
+    return s.replace("/", "-").replace("\t", " ")
+
+
+def get_filename(timestamp, title=""):
+    if title:
+        return timestamp
+    else:
+        return timestamp + " - " + escape(title)
+    
+def mkdir_exist_ok(*args, **kwargs):
+    with suppress(FileExistsError):
+        mkdir(*args, **kwargs)
+
+def non_nullify(it):
+    # let's just hope no one names their files empty strings lol
+    return (x if x is not None else "" for x in it)
+
+def iso8601(ts):
+    return datetime.datetime.fromtimestamp(ts).isoformat() + "Z"
